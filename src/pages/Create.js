1df0ee8aca50e1da34bc5bf5f5ecf42c51662194
cmjiwa <wangjinghuan@yahoo.com>
2022-08-01 14:44:03 +0300

    ButtonGroup

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,6 +2,11 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
+//the same below 2 importing method, also for other components like Typography
+//for example, it is that the way wihout curely braces has a little bit better performance.
+//import {ButtonGroup} from "@material-ui/core";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+
 export default function Create() {
   return (
     <div>
@@ -17,6 +22,12 @@ export default function Create() {
       <Button type="submit" color="secondary" variant="outlined">
         submit
       </Button>
+
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
     </div>
   );
 }
